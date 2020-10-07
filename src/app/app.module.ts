7f146067bf2689d5a3dda4041c6fad7d9fc9b512
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-10-07 12:08:00 -0500

    Se agrega lista de reproducción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/template/navbar/navbar.component';
@@ -18,6 +18,7 @@ import { FooterComponent } from './components/template/footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { YoutubeService } from '../servicios/youtube.service';
+import {Video} from '../models/youtube.models';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private youtubeService:YoutubeService) { }
+  videos: Video[] = [];
   ngOnInit(): void {
+    this.youtubeService.getVideos()
+    .subscribe(resp=>{
+      this.videos.push(...resp);
+      console.log(this.videos);
+    })
   }
 
 }
